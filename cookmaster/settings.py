eb51fe7952be171b975379afff148f207e6ff2d8
Tyler Stuessi <tyler.stuessi@gmail.com>
2016-10-27 18:25:34 -0400

    Add whitenoise

--- cookmaster/settings.py
+++ cookmaster/settings.py
@@ -113,10 +113,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
-PROJECT_DIR = BASE_DIR
+#PROJECT_DIR = BASE_DIR
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIR = (
-        os.path.join(BASE_DIR, 'static')
-)
+#STATICFILES_DIR = (
+#        os.path.join(BASE_DIR, 'static')
+#)
+
+# serve static files using whitenoise
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
