9a374aabd05fbf48823e022aabf0789504d8eed3
Tyler Stuessi <tyler.stuessi@gmail.com>
2016-10-27 18:18:34 -0400

    Update Secret

--- cookmaster/settings.py
+++ cookmaster/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'l@vhnid=#)qf2#5x4#wnqliep2axiypwpj63ilby0)3^v9%jsz'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
