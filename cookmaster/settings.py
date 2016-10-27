699e5722d3b86955679c17c262a01cd88d1d9ef5
Tyler Stuessi <tyler.stuessi@gmail.com>
2016-10-27 18:08:06 -0400

    Update settings

--- cookmaster/settings.py
+++ cookmaster/settings.py
@@ -113,6 +113,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
+PROJECT_DIR = BASE_DIR
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
